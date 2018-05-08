b267e19d8e78fc58f3fd27a26f3a25613efc9b8d
Matthew Brahms <matthewbrahms@gmail.com>
2018-05-07 21:59:40 -0500

    intro to pointers!

--- 03-structs/main.go
+++ 03-structs/main.go
@@ -12,7 +12,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
@@ -33,11 +33,23 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmailz.com",
 			zipCode: 94000,
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+
+	// fmt.Printf("%+v", jim)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
