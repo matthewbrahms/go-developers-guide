983c0449667377773bf858f9eb953a51fe692658
Matthew Brahms <matthewbrahms@gmail.com>
2018-05-13 22:50:34 -0500

    working with readers and writers

--- 07-http/main.go
+++ 07-http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	r, err := http.Get("http://google.com")
@@ -13,5 +16,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(r)
+	// xb := make([]byte, 99999)
+	// r.Body.Read(xb)
+	// fmt.Println(string(xb))
+
+	lw := logWriter{}
+
+	io.Copy(lw, r.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
